8a7f118bd014f87e0385350baf57ac90e4d10e2c
eric.njogu <eric.njogu@bringglobal.com>
2022-05-09 10:06:45 +0300

    initial commit

---
+++ addon/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'harmony_io_college',
+    'summary': 'College courses & enrollment',
+    'description': """
+    Manage college courses as services and track fees using invoices
+    """,
+    'category': 'education',
+    'dependes': ['invoicing'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ addon/readme.md
@@ -0,0 +1,7 @@
+# Proffesional Courses
+This is an odoo addon for managing proffessional courses at a college.
+
+It is tightly integrated with the invoicing and products modules.
+
+## Features
+- Enrollment
